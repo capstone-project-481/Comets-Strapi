a3fd7f893430909f306348db5d528bc3cc2346d9
maslindc2 <maslinfar@gmail.com>
2022-02-18 11:01:23 -0800

    changed to verified single sender email

--- config/plugins.js
+++ config/plugins.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
           apiKey: env('SENDGRID_API_KEY'),
         },
         settings: {
-          defaultFrom: 'maslinfar@gmail.com',
-          defaultReplyTo: 'maslinfar@gmail.com',
-          testAddress: 'maslinfar@gmail.com',
+          defaultFrom: 'farrellmas@cwu.edu',
+          defaultReplyTo: 'farrellmas@cwu.edu',
+          testAddress: 'qtelnbiikabtzwlgxx@nthrw.com',
         },
       },
     },
